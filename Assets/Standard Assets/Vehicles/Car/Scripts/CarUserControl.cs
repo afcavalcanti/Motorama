c4233b1ca772d73a4d4fdaeb4d7fb8d658ff1a65
afcavalcanti <afcavalcanti99@gmail.com>
2018-02-19 10:45:53 -0300

    first commit

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        float h;
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+        /*
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "CurvaEsquerda")
+            {
+                h = -0.2f;
+            }
+            if (other.tag == "Reta")
+            {
+                h = 0;
+            }
+        }*/
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
